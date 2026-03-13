58014b425ccdae0ae75373e0bf6b09fae26edcf1
Eduard Gacea <eduardgacea@gmail.com>
2026-03-13 20:43:42 +0200

    continuare LSI

---
+++ 04_LSI/masini.c
@@ -0,0 +1,252 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define BUFFER_SIZE 128
+
+typedef struct Masina Masina;
+typedef struct Nod Nod;
+typedef struct Lista Lista;
+
+struct Masina
+{
+    int id;
+    int nrUsi;
+    float pret;
+    char *model;
+    char *numeSofer;
+    unsigned char serie;
+};
+
+struct Nod
+{
+    Masina *masina;
+    Nod *next;
+};
+
+struct Lista
+{
+    Nod *prim;
+};
+
+Masina *creazaMasina(int id, int nrUsi, float pret, const char *model, const char *numeSofer, unsigned char serie);
+Nod *creazaNod(Masina *m);
+Lista *creazaLista();
+Masina *creazaMasinaFisier(FILE *f);
+void adaugaNodLaFinal(Lista *l, Nod *n);
+Lista *creazaListaFisier(FILE *f);
+void afiseazaMasina(Masina *m);
+void afiseazaLista(Lista *l);
+void dezalocaMasina(Masina **m);
+void dezalocaLista(Lista **l);
+
+int main()
+{
+    FILE *f = fopen("masini.txt", "r");
+    if (!f)
+        return 1;
+
+    Lista *lista = creazaListaFisier(f);
+    afiseazaLista(lista);
+
+    dezalocaLista(&lista);
+    afiseazaLista(lista);
+
+    fclose(f);
+
+    return 0;
+}
+
+Masina *creazaMasina(int id, int nrUsi, float pret, const char *model, const char *numeSofer, unsigned char serie)
+{
+    Masina *m = malloc(sizeof(Masina));
+    if (!m)
+        return NULL;
+
+    const char *sursaModel = model ? model : "necunoscut";
+    m->model = malloc(strlen(sursaModel) + 1);
+    if (!m->model)
+    {
+        free(m);
+        return NULL;
+    }
+    strcpy_s(m->model, strlen(sursaModel) + 1, sursaModel);
+
+    const char *sursaNumeSofer = numeSofer ? numeSofer : "necunoscut";
+    m->numeSofer = malloc(strlen(sursaNumeSofer) + 1);
+    if (!m->numeSofer)
+    {
+        free(m->model);
+        free(m);
+        return NULL;
+    }
+    strcpy_s(m->numeSofer, strlen(sursaNumeSofer) + 1, sursaNumeSofer);
+
+    m->id = id;
+    m->nrUsi = nrUsi;
+    m->pret = pret;
+    m->serie = serie;
+
+    return m;
+}
+
+Nod *creazaNod(Masina *m)
+{
+    if (!m)
+        return NULL;
+
+    Nod *nod = malloc(sizeof(Nod));
+    if (!nod)
+        return NULL;
+
+    nod->masina = m;
+    nod->next = NULL;
+
+    return nod;
+}
+
+Lista *creazaLista()
+{
+    Lista *lista = malloc(sizeof(Lista));
+    if (!lista)
+        return NULL;
+
+    lista->prim = NULL;
+
+    return lista;
+}
+
+Masina *creazaMasinaFisier(FILE *f)
+{
+    if (!f)
+        return NULL;
+
+    char buffer[BUFFER_SIZE];
+    int id, nrUsi;
+    float pret;
+    char *token;
+    unsigned char serie;
+
+    char *mData = fgets(buffer, BUFFER_SIZE, f);
+    if (!mData)
+        return NULL;
+
+    token = strtok(mData, ",");
+    id = token ? atoi(token) : 0;
+    token = strtok(NULL, ",");
+    nrUsi = token ? atoi(token) : 0;
+    token = strtok(NULL, ",");
+    pret = token ? atof(token) : 0.0f;
+    token = strtok(NULL, ",");
+    char *model = token ? token : "necunoscut";
+    token = strtok(NULL, ",");
+    char *numeSofer = token ? token : "necunoscut";
+    token = strtok(NULL, ",");
+    serie = token ? token[0] : '?';
+
+    return creazaMasina(id, nrUsi, pret, model, numeSofer, serie);
+}
+
+void adaugaNodLaFinal(Lista *l, Nod *n)
+{
+    if (!l || !n)
+        return;
+
+    if (l->prim == NULL)
+    {
+        l->prim = n;
+        return;
+    }
+
+    Nod *p = l->prim;
+    while (p->next)
+    {
+        p = p->next;
+    }
+
+    p->next = n;
+    n->next = NULL;
+}
+
+Lista *creazaListaFisier(FILE *f)
+{
+    if (!f)
+        return NULL;
+
+    Lista *l = creazaLista();
+    if (!l)
+        return NULL;
+
+    Masina *m;
+
+    while ((m = creazaMasinaFisier(f)) != NULL)
+    {
+        Nod *n = creazaNod(m);
+        adaugaNodLaFinal(l, n);
+    }
+
+    return l;
+}
+
+void afiseazaMasina(Masina *m)
+{
+    if (!m)
+    {
+        printf("masina neinitializata\n");
+        return;
+    }
+
+    printf("masina #%d %s %c cu %d usi, condusa de %s, costa %.2f RON\n", m->id, m->model ? m->model : "necunoscut", m->serie, m->nrUsi, m->numeSofer ? m->numeSofer : "necunoscut", m->pret);
+}
+
+void afiseazaLista(Lista *l)
+{
+    if (!l)
+    {
+        printf("lista neinitializata\n");
+        return;
+    }
+
+    Nod *p = l->prim;
+    while (p)
+    {
+        afiseazaMasina(p->masina);
+        p = p->next;
+    }
+}
+
+void dezalocaMasina(Masina **m)
+{
+    if (!m || !*m)
+        return;
+
+    free((*m)->model);
+    (*m)->model = NULL;
+
+    free((*m)->numeSofer);
+    (*m)->numeSofer = NULL;
+
+    free(*m);
+    *m = NULL;
+}
+
+void dezalocaLista(Lista **l)
+{
+    if (!l || !*l)
+        return;
+
+    Nod *p = (*l)->prim;
+
+    while (p)
+    {
+        Nod *temp = p->next;
+        dezalocaMasina(&p->masina);
+        free(p);
+
+        p = temp;
+    }
+
+    free(*l);
+    *l = NULL;
+}
